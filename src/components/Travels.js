22a78b233bfe9fd30731fabac89a351ff1194dd9
Psalmcee <100202765+Psalmcee@users.noreply.github.com>
2022-07-17 01:37:55 +0100

    added anchor link tag
    added link tag that enables the url of the map of the locations to open in a new tab

--- src/components/Travels.js
+++ src/components/Travels.js
@@ -1,6 +1,8 @@
 
 import locationPic from "../images/location1.png"
 export default function Travels(props) {
+
+
     return (
         <div className="travel">
             <img src={props.data.image} alt={props.data.image} />
@@ -8,8 +10,10 @@ export default function Travels(props) {
             <div className="travel--details">
             <p>
             <img src={locationPic} width="20" height="15" alt={locationPic} />
-            {props.data.country} 
-            <span className="travel--maps" onClick={props.data.map}><u> View on Google Maps</u></span>
+                {props.data.country} 
+                <span className="travel--maps" >
+                <u><a href={props.data.map} target="_blank"> View on Google Maps </a></u>
+                </span>
             </p>
             <h1>{props.data.place}</h1>
             <h3>{props.data.date}</h3>
